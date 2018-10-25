398ef3861be8784095b121d1be6cf72b529c3494
Bryan M <janbryanmartirez@gmail.com>
2018-10-25 17:46:30 +0800

    home

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import axios from 'axios'
 
-const Home = () => {
+class Home extends Component {
 
-    return (
-        <div>
-            Home Awyis
-        </div>
-    );
-};
+    state = {
+        Users: {}
+    }
 
-export default Home;
+    componentDidMount() {
+        axios.get('/api/user/get_users')
+        .then(response => {
+            let Awyis = {}
+
+            response.data.forEach((val, i)=> {
+                let tempObj = {}
+                tempObj['email'] = val.email
+                tempObj['name'] = val.name
+                tempObj['lastname'] = val.lastname
+
+                Awyis[`User${i}`] = tempObj
+            })
+
+            this.setState({
+                Users: Awyis
+            })            
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <div>
+                    Home Awyis
+                </div>
+                <div>         
+                    {JSON.stringify(this.state.Users, null, 1)}
+                </div>
+            </div>
+        );
+    }
+}
+export default Home
